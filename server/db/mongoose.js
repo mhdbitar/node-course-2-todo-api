b33b2615e1ac6fc3bf393a0f1c824f6f01cc3a8a
m.bitar <m.bitar@aalami.me>
2018-12-05 23:35:59 +0300

    'add'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://mhd:6218549B@ds127094.mlab.com:27094/todoapp' ||
-    'mongodb://localhost:27017/TodoApp');
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// 'mongodb://mhd:6218549B@ds127094.mlab.com:27094/todoapp'
 module.exports = {
     mongoose
 };
