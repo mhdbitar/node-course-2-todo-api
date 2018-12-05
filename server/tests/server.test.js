998894727f47472c2e4e1d3d4b85261d337785b0
m.bitar <m.bitar@aalami.me>
2018-12-05 22:36:54 +0300

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,8 @@
 const expect = require('expect');
 const request = require('supertest');
+const {
+    ObjectId
+} = require('mongodb');
 
 const {
     app
@@ -9,8 +12,10 @@ const {
 } = require('./../modules/todo');
 
 const todos = [{
+    _id: new ObjectId(),
     text: 'First test todo'
 }, {
+    _id: new ObjectId(),
     text: 'Second test todo'
 }];
 
@@ -76,4 +81,31 @@ describe('GET /todos', () => {
             })
             .end(done);
     });
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text)
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var hexId = new ObjectId().toHexString();
+        request(app)
+            .get(`/todos/${hexId}}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .get(`/todos/123}`)
+            .expect(404)
+            .end(done);
+    })
 });
