525d6dd0afc3f7a3dd7cfdf566936452b39aa0de
m.bitar <m.bitar@aalami.me>
2018-12-05 23:19:13 +0300

    'add'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<mhd>:<6218549B>@ds127094.mlab.com:27094/todoapp');
+mongoose.connect('mongodb://mhd:6218549B@ds127094.mlab.com:27094/todoapp');
 
 module.exports = {
     mongoose
