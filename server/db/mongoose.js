c4f0cb0e98181af31d18db2229c29e1384f54205
m.bitar <m.bitar@aalami.me>
2018-12-07 01:33:45 +0300

    Add PATCH /todos:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-// 'mongodb://mhd:6218549B@ds127094.mlab.com:27094/todoapp'
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://mhd:6218549B@ds127094.mlab.com:27094/todoapp');
 module.exports = {
     mongoose
 };
