f8c1697d8218b1d2cc6c31de0853d7d3ef7f31ab
m.bitar <m.bitar@aalami.me>
2018-12-08 00:08:04 +0300

    Setup seprate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-// mongoose.connect('mongodb://mhd:6218549B@ds127094.mlab.com:27094/todoapp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://mhd:6218549B@ds127094.mlab.com:27094/todoapp');
 module.exports = {
     mongoose
 };
