063e1d5375d35cdb4248f7e839dfcaf326a524aa
m.bitar <m.bitar@aalami.me>
2018-12-09 02:43:40 +0300

    Add POST /users route and token method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://mhd:6218549B@ds127094.mlab.com:27094/todoapp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://mhd:6218549B@ds127094.mlab.com:27094/todoapp', {
+    useCreateIndex: true,
+    useNewUrlParser: true
+});
 module.exports = {
     mongoose
 };
